37919ecc0d41293ae583bf940f6ddf84177951d7
Kristian Sons <kristian.sons@dfki.de>
2015-11-12 14:53:25 +0100

    New version of plug-in with improved memory efficiency

--- xml3d-stl-plugin.js
+++ xml3d-stl-plugin.js
@@ -2105,19 +2105,17 @@ DataStream.flipArrayEndianness = function(array) {
 
 /**
   Creates an array from an array of character codes.
-  Uses String.fromCharCode in chunks for memory efficiency and then concatenates
-  the resulting string chunks.
+  Uses String.fromCharCode on the character codes and concats the results into a string.
 
   @param {array} array Array of character codes.
   @return {string} String created from the character codes.
 **/
 DataStream.createStringFromArray = function(array) {
-  var chunk_size = 0x8000;
-  var chunks = [];
-  for (var i=0; i < array.length; i += chunk_size) {
-    chunks.push(String.fromCharCode.apply(null, array.subarray(i, i + chunk_size)));
+  var str = "";
+  for (var i=0; i<array.length; i++) {
+    str += String.fromCharCode(array[i]);
   }
-  return chunks.join("");
+  return str;
 };
 
 /**
